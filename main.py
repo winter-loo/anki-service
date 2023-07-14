39ade4f8650c454dbba706c1a114dee3a5ff68ed
adjkldd <adjkldd@126.com>
2023-07-14 08:38:30 +0800

    use more api

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ bk = RustBackend(['en'], True)
 
 bk.open_collection(collection_path='instance/collection.anki2', media_folder_path='instance/collection.media', media_db_path='instance/collection.media.db2', force_schema11=False)
 
-c = bk.get_queued_cards(fetch_limit=1, intraday_learning_only=True)
-print(c)
+# ADUQ: Add, Delete, Update, Query
+# ADUQ with deck
+
+a = bk.new_deck()
+a.name = "test"
+deck = bk.add_deck(a)
+bk.get_deck(deck.id)
+# bk.remove_decks([deck.id])
+
+
+# ADUQ with note
+a = bk.get_notetype_names()
+basic_note_type = a[0]
+nn = bk.new_note(basic_note_type.id)
+nn.fields[0] = "front1"
+nn.fields[1] = "back1"
+bk.add_note(note=nn, deck_id=deck.id)
